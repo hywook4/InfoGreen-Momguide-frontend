a6ba61e1515e7be7229e67559f4429e40353c98e
Jung Hyun Wook <hywook4@naver.com>
2019-01-18 22:17:15 +0900

    search bar design fixed, menubar fixing

--- src/components/content/search/Search.js
+++ src/components/content/search/Search.js
@@ -35,15 +35,14 @@ class Search extends React.Component {
 
         return(
             <div className="search-div">
-                <div className="container">
-                <div className="search_heading">사용 중이신 브랜드명 혹은, 제품명을 검색하여 유해성분이 있는지 찾아보세요</div>
+                <div className="search_container">
+                    <div className="search_heading">사용 중이신 브랜드명 혹은, 제품명을 검색하여 유해성분이 있는지 찾아보세요</div>
                     <div className="search_box">
                         <input 
                             type="text"
                             placeholder="총 400,000개의 제품..."
                             value={this.state.search}
                             onChange={this.onChange}
-                            onFocus={this.onFocus}
                         />
                         <i className="fa fa-search" aria-hidden="true"></i>
                     </div>
