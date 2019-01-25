4387bf2a00fdbbbcbc9b9ef79c483b4716f5dcb1
Jung Hyun Wook <hywook4@naver.com>
2019-01-25 17:21:06 +0900

    check box style fixing

--- src/components/content/navigation/category/Category.js
+++ src/components/content/navigation/category/Category.js
@@ -136,24 +136,24 @@ export class Category extends React.Component{
     renderRenderChecboxSelector=()=>{
         return(
             <div className="checkbox-div">
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="defaultInline1" name="1" onClick={this.onCheck}/>
+                <div className="check-element">
+                    <input type="checkbox" id="defaultInline1" name="1" onClick={this.onCheck}/>
                     <label htmlFor="defaultInline1" className="custom-control-label">성분 공개 제품</label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="defaultInline2"name="3" onClick={this.onCheck}/>
+                <div className="check-element">
+                    <input type="checkbox" id="defaultInline2" name="3" onClick={this.onCheck}/>
                     <label htmlFor="defaultInline2" className="custom-control-label">주의 성분 제외</label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="defaultInline3" name="2" onClick={this.onCheck}/>
+                <div className="check-element">
+                    <input type="checkbox" id="defaultInline3" name="2" onClick={this.onCheck}/>
                     <label htmlFor="defaultInline3" className="custom-control-label">유해 성분 제외</label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="defaultInline4" name="4" onClick={this.onCheck}/>
+                <div className="check-element">
+                    <input type="checkbox" id="defaultInline4" name="4" onClick={this.onCheck}/>
                     <label htmlFor="defaultInline4" className="custom-control-label">높은 위험도 성분 제외</label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="defaultInline5" name="5" onClick={this.onCheck}/>
+                <div className="check-element">
+                    <input type="checkbox" id="defaultInline5" name="5" onClick={this.onCheck}/>
                     <label htmlFor="defaultInline5" className="custom-control-label">친환경 인증 제품</label>
                 </div>
             </div>
