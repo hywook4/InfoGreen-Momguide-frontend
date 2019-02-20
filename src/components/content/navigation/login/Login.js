c5214b56088048b38b1a503e33dac1fad1611947
SeoNamgyu <zych1751@naver.com>
2019-02-20 23:18:25 +0900

    login banner image 스타일 수정

--- src/components/content/navigation/login/Login.js
+++ src/components/content/navigation/login/Login.js
@@ -9,7 +9,7 @@ export class Login extends React.Component{
             <div className="container">
            
                 <div className="login-container">
-                    <img src={LOGIN_BANNER} alt=""></img>
+                    <img src={LOGIN_BANNER} alt="" className="login-banner-image" />
                     <div className="login-info">
                         맘가이드 홈페이지를 방문해주셔서 감사합니다.<br />
                         로그인하시면 홈페이지를 보다 편리하게 이용하실 수 있습니다.
