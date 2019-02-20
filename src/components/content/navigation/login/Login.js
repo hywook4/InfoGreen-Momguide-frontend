a5e6e5087e25ffb2bc82a7af68a6219e325b4660
SeoNamgyu <zych1751@naver.com>
2019-02-21 05:42:16 +0900

    문의하기 페이지에서 비 로그인 시 다시 로그인을 하고 redirect 구현

--- src/components/content/navigation/login/Login.js
+++ src/components/content/navigation/login/Login.js
@@ -7,14 +7,13 @@ export class Login extends React.Component{
     render(){
         return(
             <div className="container">
-           
                 <div className="login-container">
                     <img src={LOGIN_BANNER} alt="" className="login-banner-image" />
                     <div className="login-info">
                         맘가이드 홈페이지를 방문해주셔서 감사합니다.<br />
                         로그인하시면 홈페이지를 보다 편리하게 이용하실 수 있습니다.
                     </div>
-                    <LoginInput />
+                    <LoginInput nextPath='/'/>
                 </div>
             </div>
         )
--- src/components/content/navigation/request/loggedIn/LoggedIn.js
+++ src/components/content/navigation/request/loggedIn/LoggedIn.js
@@ -1,34 +1,110 @@
 import React from 'react';
 import './LoggedIn.css';
-
+import { TokenUtil } from '../../../../../util';
+import axios from 'axios';
+import history from '../../../../../history/history';
+import $ from 'jquery';
+import LoginInput from '../../login/LoginInput';
 
 export class LoggedIn extends React.Component{
-    render=()=>{
-    return(
-        <React.Fragment>
-        <div className="loggedin">
-            <div className="help-row">
-                <a href="/request/request-comment" className="ingr-anchor">
-                    <div className="ingr-anly">
-                        <div className="inr-ingr">
-                            <h2>성분 분석 요청하기</h2>
-                            <p><small>검색해도 나오지 않는 제품이 있다면?</small></p>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginView: false
+        }
+    }
+
+    handleLogin = async (token) => {
+        try {
+            const data = await axios({
+                method: 'get',
+                headers: TokenUtil.getTokenRequestHeader(token),
+                url: `${process.env.API_URL}/api/auth/info`
+            });
+            return data;
+        } catch(err) {
+            console.log(err);
+            return null;
+        }
+    };
+
+    handleLink = async (e) => {
+        e.preventDefault();
+        const nextPath = e.currentTarget.pathname;
+
+        const token = TokenUtil.getLoginToken();
+        if(token === null) {
+            this.setState({
+                loginView: true,
+                nextPath: nextPath
+            });
+            return;
+        }
+
+        const loginResponse = await this.handleLogin(token);
+        if(loginResponse === null) {
+            this.setState({
+                loginView: true,
+                nextPath: nextPath
+            });
+            return;
+        }
+
+        history.push(nextPath);
+    };
+
+    componentDidUpdate() {
+        if(this.state.loginView) {
+            $('#loginModalCenter').modal();
+        }
+    }
+
+    render() {
+        const loginModal = this.state.loginView ?
+            (<div>
+                <div className="modal fade" id="loginModalCenter" tabIndex="-1" role="dialog" aria-labelledby="loginModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <LoginInput nextPath={this.state.nextPath} modalId={'loginModalCenter'}/>
+                            </div>
                         </div>
                     </div>
-                </a>
-                <a href="/request/contact-us" className="ingr-anchor">
-                    <div className="ingr-anly">
-                        <div className="inr-ingr">                           
-                            <h2>1:1 문의하기</h2>
-                            <p><small>문의사항이나 MomGuide에게 바라는 점이 있다면?</small></p>                         
-                        </div>
+                </div>
+            </div>) :
+            null;
+        return(
+            <React.Fragment>
+                <div className="loggedin">
+                    <div className="help-row">
+                        <a href="/request/request-comment" className="ingr-anchor" onClick={this.handleLink}>
+                            <div className="ingr-anly">
+                                <div className="inr-ingr">
+                                    <h2>성분 분석 요청하기</h2>
+                                    <p><small>검색해도 나오지 않는 제품이 있다면?</small></p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="/request/contact-us" className="ingr-anchor" onClick={this.handleLink}>
+                            <div className="ingr-anly">
+                                <div className="inr-ingr">
+                                    <h2>1:1 문의하기</h2>
+                                    <p><small>문의사항이나 MomGuide에게 바라는 점이 있다면?</small></p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </a> 
-            </div>
-        </div>
-        </React.Fragment>
-    )
-}
+                    {loginModal}
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 
