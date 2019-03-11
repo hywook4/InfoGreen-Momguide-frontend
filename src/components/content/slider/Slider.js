bb4c66da0fd7edde28fe56013610ffa33f885c36
Jung Hyun Wook <hywook4@naver.com>
2019-03-11 15:51:34 +0900

    slider api connection

--- src/components/content/slider/Slider.js
+++ src/components/content/slider/Slider.js
@@ -2,49 +2,71 @@ import React from 'react';
 import '../../../../node_modules/react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 import './Slider.css';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-export const Slider = ()=>{
-    return (
-        <div className="slider main-banner-slider-container">
-            <div className="slider-inner">
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="4000">
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        {/*<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>*/}
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active main-banner-slider">
-                            <Link to="/request/contact-us">
-                                <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="First Slide" />
-                            </Link>
-                        </div> 
-                        {/*<div className="carousel-item main-banner-slider">
-                            <Link to="/request/contact-us">
-                                <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="Second Slide" />
-                            </Link>
-                        </div> 
-                        <div className="carousel-item main-banner-slider">
-                            <Link to="/request/contact-us">
-                                <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="Third Slide" />
-                            </Link>
-                        </div>*/} 
+
+export class Slider extends React.Component {
+
+    state = {
+        sliders: []
+    }
+
+    
+    componentDidMount = () => {
+    
+        axios.get(`${process.env.API_URL}/api/main/slider`)
+        .then((res) => {
+            this.setState({
+                sliders: res.data
+            })
+        });
+    }
+
+
+    render() {
+    
+
+        return (
+            <div className="slider main-banner-slider-container">
+                <div className="slider-inner">
+                    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="4000">
+                        <ol className="carousel-indicators">
+                            {
+                                this.state.sliders.map((d, i) => {
+                                    return (
+                                        <li data-target="#carouselExampleIndicators" data-slide-to={i} className={i === 0 ? "active" : ""}></li>
+                                    )
+                                })
+                            }
+                           
+                        </ol>
+                        <div className="carousel-inner">
+                            {
+                                this.state.sliders.map((d, i) => {
+                                    return(
+                                        <div className={`carousel-item main-banner-slider ${i === 0 ? "active" : ""}`}>
+                                            <a href={d.linkUrl} target="_blank">
+                                                <img src={d.imageUrl} className="d-block w-100" alt={`Slide ${d.index}`} />
+                                            </a>
+                                        </div> 
+                                    )
+                                })
+                            }
+
+                        </div>    
+                    
+                        <a href="#carouselExampleIndicators" className="carousel-control-prev" role="button" data-slide="prev">
+                            <span carousel="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a href="#carouselExampleIndicators" className="carousel-control-next" role="button" data-slide="next">
+                            <span carousel="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
                     </div>    
-                
-                    <a href="#carouselExampleIndicators" className="carousel-control-prev" role="button" data-slide="prev">
-                        <span carousel="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a href="#carouselExampleIndicators" className="carousel-control-next" role="button" data-slide="next">
-                        <span carousel="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>    
-
-            </div>   
-        </div>    
- 
-       
-    );
-
-};
+    
+                </div>   
+            </div>    
+        )
+    }
+}
