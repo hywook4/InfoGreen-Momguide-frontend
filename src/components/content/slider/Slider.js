76a88252243fd96fa2f89c0b9165ee132e209acd
Jung Hyun Wook <hywook4@naver.com>
2019-03-11 10:36:40 +0900

    slider example added

--- src/components/content/slider/Slider.js
+++ src/components/content/slider/Slider.js
@@ -10,7 +10,8 @@ export const Slider = ()=>{
                 <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="4000">
                     <ol className="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        
+                        {/*<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>*/}
                     </ol>
                     <div className="carousel-inner">
                         <div className="carousel-item active main-banner-slider">
@@ -18,7 +19,16 @@ export const Slider = ()=>{
                                 <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="First Slide" />
                             </Link>
                         </div> 
-                        
+                        {/*<div className="carousel-item main-banner-slider">
+                            <Link to="/request/contact-us">
+                                <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="Second Slide" />
+                            </Link>
+                        </div> 
+                        <div className="carousel-item main-banner-slider">
+                            <Link to="/request/contact-us">
+                                <img src="https://s3.ap-northeast-2.amazonaws.com/infogreenmomguide/mainpage-banner/banner-1.jpg" className="d-block w-100" alt="Third Slide" />
+                            </Link>
+                        </div>*/} 
                     </div>    
                 
                     <a href="#carouselExampleIndicators" className="carousel-control-prev" role="button" data-slide="prev">
